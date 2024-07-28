fd49824d7a27462b65f18850854bcfff8b577bf6
shenghuo2 <1308357113@qq.com>
2024-07-28 16:58:56 +0000

    🎉 init: at Layout-4
    学习进度：https://docs.astro.build/zh-cn/tutorial/4-layouts/1/

---
+++ src/scripts/menu.js
@@ -0,0 +1,7 @@
+document.querySelector('.hamburger').addEventListener('click', () => {
+    document.querySelector('.nav-links').classList.toggle('expanded');
+  });
+//   document.getElementById("t2").innerHTML="段落已修改。1";
+//   document.getElementById("demo").innerHTML="段落已修改2。";
+
+  console.log('Hi! 我是客户端。这是在浏览器的开发控制台中记录的');
